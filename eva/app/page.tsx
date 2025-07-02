77e1e703f17441846759699261466d239b7f3334
Zyrozk <carofer1239@gmail.com>
2025-07-01 22:16:49 -0400

    Interfaces y almacenar valor indicado
    Se agregó la interfaz para los 3 campos y asignarles sus valores (int, str). También, los const para que se ingresen los parámetros y que inicie vacío.

--- eva/app/page.tsx
+++ eva/app/page.tsx
@@ -1,3 +1,55 @@
+interface Evento{
+  nombreEven : string,
+  fecha : string,
+  direccion : string  
+}
+
+interface Beneficiario{
+  nombre : string,
+  apellido : string,
+  telefono : number,
+  rol : string
+}
+
+interface Proyecto{
+  nombreProye : string,
+  objetivo : string,
+  personaAcargo : string
+}
+
+const initialStateEvento:Evento = {
+  nombreEven : "",
+  fecha : "",
+  direccion : ""
+}
+
+const initialStateBeneficiario:Beneficiario = {
+  nombre : "",
+  apellido : "",
+  telefono : 0,
+  rol : ""
+}
+
+const initialStateProyecto:Proyecto = {
+  nombreProye : "",
+  objetivo : "",
+  personaAcargo : ""
+}
+
+const handleRegistrar = ()=>{
+  alert("Falta")
+}
+
+const handleActualizar = ()=>{
+  alert("Falta")
+}
+
+const handleEliminar = ()=>{
+  alert("Falta")
+}
+
+
+
 return (
     <form>
       <h1>Eventos</h1>
