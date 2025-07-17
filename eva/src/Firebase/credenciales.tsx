9f8120de07c7571441724ea909acabbd68ac7409
Zyrozk <carofer1239@gmail.com>
2025-07-16 23:09:24 -0400

    Conexión a Firebase
    Creo que conecte "bien" mi proyecto a firebase, olvide como y no estoy segura si lo que hice funciona, aparte cree el branch.

---
+++ eva/src/Firebase/credenciales.tsx
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAuhbVIlMk1gxYvnD8vZQefmbHLPmBaNM8",
+  authDomain: "evaluacion4v.firebaseapp.com",
+  projectId: "evaluacion4v",
+  storageBucket: "evaluacion4v.firebasestorage.app",
+  messagingSenderId: "812436568263",
+  appId: "1:812436568263:web:5e36f82c284efcc88126e2",
+  measurementId: "G-74SYEK19EM"
+};
+
